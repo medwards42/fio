8fc9f41d425af9d8188f1bb3842faa994ba613ae
Mike Edwards <mike@songbirdnest.com>
2013-09-08 13:30:54 -0700

    Refactored a bunch of stuff.

--- fio.rb
+++ fio.rb
@@ -17,7 +17,10 @@ end
 
 not_found do
   status 404
-  'These are not the droids you\'re looking for.'
+  'These are not the droids you\'re looking for.' +
+  '<br>It would appear that you either forgot to add an extension as a parameter,' +
+  '<br>or you did not post any data for processing.' +
+  '<br><br>In any case you should probably refer to the README located <a href="https://github.com/medwards42/fio" target="_blank"> here</a>.'
 end
 
 get '/health_check' do
@@ -26,17 +29,12 @@ get '/health_check' do
 end
 
 get '/' do 
-  status 200
-  content_type :json
-  @title = "fio - Speed Dial"
-  @f = JSON.parse(File.read("./public/directory.json"), :symbolize_names => true)
-  "#{@f.to_json}"
-  #erb :index
+  params.empty? ? raise(Sinatra::NotFound) : raise(Sinatra::NotFound)
 end
 
 get '/:ext' do 
   status 200
-  
+  content_type :json
   @title = "fio - Speed Dial"
   @f = JSON.parse(File.read("./public/directory.json"), :symbolize_names => true)
   @ext = params[:ext].to_s
